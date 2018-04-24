cd9844329bf13dc1cea1c264136e85df733263eb
yangdengxueshi <1437977371@qq.com>
2018-04-24 15:54:44 +0800

    优化CDRWifiUDPReceiver!

--- app/src/main/java/com/dexin/ad_system/cdr/CDRWifiUDPReceiver.java
+++ app/src/main/java/com/dexin/ad_system/cdr/CDRWifiUDPReceiver.java
@@ -8,32 +8,35 @@ import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 
 /**
- * 接收CDR 发出的 Wifi UDP 数据包
+ * CDRWifiUDPReceiver   接收CDR 发出的 Wifi UDP 数据包
  */
 public class CDRWifiUDPReceiver {
-    private static final String TAG = "TAG_接收CDR Wifi UDP包";
-    private DatagramSocket datagramSocket;          //单播套接字
-    private DatagramPacket datagramPacket;
+    private static final String TAG = "TAG_接收CDR_Wifi_UDP包";
+    private DatagramSocket mDatagramSocket;          //单播套接字
+    private DatagramPacket mDatagramPacket;
     private boolean isNeedReceiveUDP = false;       //是否需要接收UDP数据包（没有启动的时候不需要接收）
 
+    /**
+     * 接收CDR_Wifi_UDP数据包
+     */
     public void receiveCDRWifiUDPPacket() {
-        LogUtil.d(TAG, "################################################开始接收CDR Wifi UDP 数据包################################################");
-        isNeedReceiveUDP = true;                    //开始接受CDR Wifi UDP 数据包
+        LogUtil.d(TAG, "################################################ 开始接收CDR_Wifi_UDP 数据包 ################################################");
         try {
-            if (datagramSocket == null) {
-                datagramSocket = new DatagramSocket(null);
-                datagramSocket.setReuseAddress(true);
-                datagramSocket.bind(new InetSocketAddress(AppConfig.mPort));
+            isNeedReceiveUDP = true;                    //开始接受CDR_Wifi_UDP 数据包
+            if (mDatagramSocket == null) {
+                mDatagramSocket = new DatagramSocket(null);
+                mDatagramSocket.setReuseAddress(true);//地址复用
+                mDatagramSocket.bind(new InetSocketAddress(AppConfig.mPort));//端口
             }
-            byte[] udpContainer = new byte[AppConfig.mUDPPacketSize];
+            byte[] udpContainer = new byte[AppConfig.mUDPPacketSize];//1460包长
+            if (mDatagramPacket == null) mDatagramPacket = new DatagramPacket(udpContainer, udpContainer.length);//建立一个指定缓冲区大小的数据包
             while (isNeedReceiveUDP) {
-                datagramPacket = new DatagramPacket(udpContainer, udpContainer.length);      //建立一个指定缓冲区大小的数据包
-//                datagramSocket.setSoTimeout();
-                datagramSocket.receive(datagramPacket);                         //将单播套接字收到的UDP数据包存放于datagramPacket中(会阻塞)
-                byte[] udpDataPacket = datagramPacket.getData();                            //UDP数据包
+//                mDatagramSocket.setSoTimeout();
+                mDatagramSocket.receive(mDatagramPacket);//将单播套接字收到的UDP数据包存放于datagramPacket中(会阻塞)
+                byte[] udpDataPacket = mDatagramPacket.getData();//UDP数据包
 //                LogUtil.d(TAG, "UDP原始数据包AAAAAAAAAAAAAAAAAAAAA：" + stringhelpers.bytesToHexString(udpDataPacket).toUpperCase());
                 if ((udpDataPacket != null) && (udpDataPacket.length > 0)) {
-                    byte[] udpPayload = CDRUtils.parseUDPPacketToPayload(udpDataPacket);      //解析UDP数据包后获得的UDP净荷
+                    byte[] udpPayload = CDRUtils.parseUDPPacketToPayload(udpDataPacket);//解析UDP数据包后获得的UDP净荷
 //                    LogUtil.d(TAG, "ts净荷包AAAAAAAAAAAAAAAAAAAAA：" + stringhelpers.bytesToHexString(udpPayload).toUpperCase());
                     if ((udpPayload != null) && (udpPayload.length > 0)) {
                         CDRWifiUDPPayloadQueue.addIntoQueue(udpPayload);              //TODO 这里为了方便后面的解析工作，传入的是UDP净荷；可能引起掉包，再接收一轮
@@ -43,19 +46,19 @@ public class CDRWifiUDPReceiver {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (datagramSocket != null) {
-                datagramPacket.setPort(0);
-                datagramSocket = null;
+            if (mDatagramSocket != null) {
+                mDatagramPacket.setPort(0);
+                mDatagramSocket = null;
             }
-            if (datagramPacket != null) {
-                datagramPacket.setLength(0);
-                datagramPacket = null;
+            if (mDatagramPacket != null) {
+                mDatagramPacket.setLength(0);
+                mDatagramPacket = null;
             }
         }
     }
 
     public void stopReceiveCDRWifiUDPPacket() {
         isNeedReceiveUDP = false;
-        LogUtil.d(TAG, "################################################CDRWifiReceive 服务关闭，结束接收单播################################################");
+        LogUtil.d(TAG, "##################################### CDRWifiReceive 服务关闭，结束接收CDR_Wifi_UDP 数据包 #######################################");
     }
 }
